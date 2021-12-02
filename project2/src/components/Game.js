1e73c636a64fa7827f383573825e3a867444fb42
Myles-Cork <38143953+Myles-Cork@users.noreply.github.com>
2021-12-02 15:24:01 -0700

    Fixed render issue in game

--- project2/src/components/Game.js
+++ project2/src/components/Game.js
@@ -201,21 +201,6 @@ class Game extends React.Component{
   }
 
   render(){
-    const guesses = [
-      {number:1,colors: [0,1,2,3]},
-      {number:2,colors: [4,5,6,7]},
-      {number:3,colors: [1,4,0,0]},
-      {number:4,colors: [2,2,4,4]},
-      {number:5,colors: [1,1,5,6]},
-      {number:6,colors: [3,3,6,6]},
-      {number:7,colors: [-1,-1,-1,-1]},
-      {number:8,colors: [-1,-1,-1,-1]},
-      {number:9,colors: [-1,-1,-1,-1]},
-      {number:10,colors: [-1,-1,-1,-1]},
-      {number:11,colors: [-1,-1,-1,-1]},
-      {number:12,colors: [-1,-1,-1,-1]}
-    ]
-
     const pickerguessstyle = {
       display:"flex",
       flexDirection:"row",
@@ -223,14 +208,13 @@ class Game extends React.Component{
       margin:"auto"
     };
 
-    // checkanswer will return a function
     return(
       <div>
         <div style={pickerguessstyle}>
-          <ColorPicker/>
-          <GuessList guesses={guesses}/>
+          <ColorPicker selectedcolor={this.state.selectedcolor} select={this.selectColor} possiblecolors={this.props.settings.possiblecolors}/>
+          <GuessList guesses={this.state.guesses} currentguess={this.state.currentguessnum} addpeg={this.addPeg}/>
         </div>
-          <FlowBar checkanswer="temp" answerdisplay={[0,1,2,3]} progress="30%"/>
+          <FlowBar checkanswer={this.checkAnswer} answerdisplay={this.state.answerdisplay} progress="30%"/>
       </div>
     );
   }
